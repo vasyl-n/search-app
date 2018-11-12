4bc9ba905f2cbdb23b088d0403f19e67d7dd165a
vasyl-n <vasyl.nrk@gmail.com>
2018-11-11 16:34:11 -0800

    adds trie, adds comments

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Search from '../search/Search'
+import Error from '../error/Error'
 import ChooseType from '../type/ChooseType'
 import data from '../../data/products.json'
 import Trie from 'trie-search'
@@ -13,43 +14,52 @@ class App extends Component {
       results: [],
       types: [],
       typesState: {},
+      clicked: false,
     }
   }
 
   componentDidMount() {
     const products = data.products
 
-    const ts = new Trie(['name'],  {indexField: 'name'});
-    ts.addAll(products);
+    const ts = new Trie(['name'],  {indexField: 'name'})
+    ts.addAll(products)
 
     this.setState({data: data.products, ts})
     const types = this.getAvailableTypes(products)
     const typesObj = this.makeTypesObject(types)
-
     this.setState({types, typesObj})
   }
 
   handleInputChange = (e) => {
     this.setState({searchVal: e.target.value})
+
+    // we could debounce this function
     this.search(e.target.value, this.state.typesObj)
   }
 
-  search = (val, typesObj) => {
-    if ( !val ) {
-      this.setState({types: [], results: []})
-      return 
-    }
-    var results = this.state.ts.get([val])
+  handleCheckboxChange = (indx) => {
+    let types = this.state.types
+    let type = types[indx]
+    type.checked = !type.checked
+    const typesObj = this.makeTypesObject(types)
+    this.setState({types, typesObj})
+    this.search(this.state.searchVal, typesObj)
+  } 
 
+  // get values from trie and filters them based on types selected
+  search = (val, typesObj) => {
+    this.setState({clicked: false})
+    var results = this.state.ts.get([val])
     results = results.filter((el) => {
       return typesObj[el.type] === true
     })
     this.setState({results})
   }
 
-  getAvailableTypes = (results) => {
+  // returns unique types from json file
+  getAvailableTypes = (products) => {
     let uniqueTypes = new Set()
-    results.forEach((el) => {
+    products.forEach((el) => {
       uniqueTypes.add(el.type)
     })
     let typesArray = Array.from(uniqueTypes)
@@ -64,15 +74,7 @@ class App extends Component {
     return types
   }
 
-  handleCheckboxChange = (indx) => {
-    let types = this.state.types
-    let type = types[indx]
-    type.checked = !type.checked
-    this.setState(types)
-    const typesObj = this.makeTypesObject(types)
-    this.search(this.state.searchVal, typesObj)
-  } 
-
+  //makes map of available types and their current values to be able to filter results
   makeTypesObject = (types) => {
     var typesObj = types.reduce((acc, el) => {
       acc[el.name] = el.checked
@@ -82,7 +84,11 @@ class App extends Component {
   }
 
   handleClick = (val) => {
-    this.setState({searchVal: val, types: [], results: []})
+    this.setState({searchVal: val, results: [], clicked: true})
+  }
+
+  checkForError = () => {
+    return this.state.searchVal !== '' && this.state.results.length === 0 && !this.state.clicked
   }
 
   render() {
@@ -93,8 +99,9 @@ class App extends Component {
           <Search handleClick={this.handleClick} value={this.state.searchVal} handleChange={this.handleInputChange} results={this.state.results} />
           <ChooseType types={this.state.types} handleChange={this.handleCheckboxChange} />
         </div>
+        <Error error={this.checkForError()}/>
       </div>
-    );
+    )
   }
 }
 
