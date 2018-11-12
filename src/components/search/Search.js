241f2637e451f80412b2a4ab4c80288a6ca0dcf8
vasyl-n <vasyl.nrk@gmail.com>
2018-11-11 17:13:50 -0800

    adds styling

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -4,13 +4,15 @@ import './Search.css'
 const Search = (props) => {
   return (
     <div className="search-container">
-      <input className="text-field" placeholder="Search keyword" onChange={props.handleChange} value={props.value}></input>
-      <div className="results">
-        {
-          props.results.map((el, indx) => {
-            return <div key={indx} onClick={() => props.handleClick(el.name)} className="result-value">{el.name}</div>
-          })
-        }
+      <div className="search-wrapper" >
+        <input className="text-field" placeholder="Search keyword" onChange={props.handleChange} value={props.value}></input>
+        <div className="results">
+          {
+            props.results.map((el, indx) => {
+              return <div key={indx} onClick={() => props.handleClick(el.name)} className="result-value">{el.name}</div>
+            })
+          }
+        </div>
       </div>
     </div>
   )
